c1d99d6fe41eb7dba532bd1b61032f75ec349c5a
osama Tubaishat <Osamanaser006@gmail.com>
2024-09-27 15:22:37 +0300

    Finish Home

--- Users/homePage/Home.js
+++ Users/homePage/Home.js
@@ -28,7 +28,9 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
-
+setInterval(function () {
+  plusSlides(1);
+}, 5000);
 //Latest events added
 let events = JSON.parse(localStorage.getItem("Event")) || []; // Use an empty array if null
 
@@ -70,7 +72,7 @@ events.push(
     title: "free medical day",
     img: ["../image/freeday.png"],
     desc: "Maintaining a healthy lifestyle is not just about exercising regularly or eating balanced meals; it's about finding a sustainable routine that promotes overall well-being.",
-    date: "12 nov 2024",
+    date: "30 sep 2024",
     location: "Jordan/Irbid",
   },
   {
@@ -94,50 +96,62 @@ events.reverse().forEach((event) => {
   card.className = "card";
   card.innerHTML = `
   <a href="../HTML/EventDetails.html?title=${event.title}&img=${event.img}&desc=${event.desc}&date=${event.date}&location=${event.location}">
-  <img src="${event.img[0]}" style="width: 100%;">
-  <h2>${event.title}</h2>
-  <br>
-  <p>${event.desc}</p>
-  <br>
-  <h4>${event.date}</h4>
-  <h4>${event.location}</h4>
+    <img src="${event.img[0]}" style="width: 100%;">
+    <h2>${event.title}</h2>
+    <p>${event.desc}</p>
+    <div class ="card-date-loc">
+      <h4>${event.location}</h4>
+      <h4>${event.date}</h4>
+    </div>
   
   </a>
 `;
-
   cardContainer.appendChild(card);
 });
 
 // starting soon
 cardContainer = document.getElementById("card-container-soon");
 let contSoon = 0;
+const today = new Date();
+const sevenDaysLater = new Date(today);
+sevenDaysLater.setDate(today.getDate() + 7);
 events.sort((a, b) => {
   const dateA = new Date(a.date);
   const dateB = new Date(b.date);
   return dateA - dateB;
 });
 events.reverse().forEach((event) => {
+  const eventDate = new Date(event.date);
   if (contSoon == 4) {
     return;
   }
-  contSoon += 1;
-  const card = document.createElement("div");
-  card.className = "card";
-  card.innerHTML = `
+  if (eventDate > today && eventDate <= sevenDaysLater) {
+    contSoon += 1;
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
     <a href="../HTML/EventDetails.html?title=${event.title}&img=${event.img}&desc=${event.desc}&date=${event.date}&location=${event.location}">
     <img src="${event.img[0]}" style="width: 100%;">
     <h2>${event.title}</h2>
-    <br>
     <p>${event.desc}</p>
-    <br>
-  <h4>${event.date}</h4>
-
-    <h4>${event.location}</h4>
+    <div class ="card-date-loc">
+      <h4>${event.location}</h4>
+      <h4>${event.date}</h4>
+    </div>
     </a>
   `;
-
-  cardContainer.appendChild(card);
+    cardContainer.appendChild(card);
+  }
 });
+let ch = cardContainer.children;
+if (ch.length == 0) {
+  const card = document.createElement("div");
+  card.className = "cardSoon";
+  card.innerHTML = `
+    <h3>Coming soon events</h3>
+`;
+  cardContainer.appendChild(card);
+}
 // nav
 function nav() {
   var x = document.getElementById("myTopnav");
